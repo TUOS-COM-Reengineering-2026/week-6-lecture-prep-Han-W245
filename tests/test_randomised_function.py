250201e2b3fa4a490bbfb46a2728f94e5bc3d6db
Han Wang <hwang245@sheffield.ac.uk>
2026-05-03 20:14:39 +0100

    Add change

--- tests/test_randomised_function.py
+++ tests/test_randomised_function.py
@@ -1,8 +1,13 @@
 import unittest
+from unittest.mock import patch
+
 from lecture import randomised_function
 
-class MyTestCase(unittest.TestCase):
 
+class MyTestCase(unittest.TestCase):
     def test_randomised_function(self):
-        self.assertEqual('software', randomised_function())  # This will pass or fail randomly
-        # TODO: Can we make this test deterministic? (HINT: Mock testing)
+        with patch("random.randint", return_value=3):
+            self.assertEqual('software', randomised_function())
+
+        with patch("random.randint", return_value=8):
+            self.assertEqual('engineering', randomised_function())
